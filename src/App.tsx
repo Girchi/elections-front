26a639bfc1103eda286d4b00a1a68e59bb489e52
dimachilaia <chilaiadima@gmail.com>
2022-12-05 00:01:16 +0400

    feat: created header component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Header from "./pages/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Header.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../images/logo.svg";
+import girchiImg from "../images/გირჩი.svg";
+
+const Header = () => {
+  return (
+    <HeaderSection>
+      <MainHeader>
+        <HeaderImgs>
+          <img src={logo} alt="logo" />
+          <img src={girchiImg} alt="girchiImg" />
+        </HeaderImgs>
+        <HeaderNavbarContainer>
+          <HeaderNavbar>
+            <li>პოლიტიკოსები</li>
+            <li>ანალიტიკა</li>
+            <li>სიახლეები</li>
+            <li>პროექტები</li>
+            <li>გაზეთი</li>
+          </HeaderNavbar>
+          <ButtonContainer>ავტორიზაცია</ButtonContainer>
+        </HeaderNavbarContainer>
+      </MainHeader>
+    </HeaderSection>
+  );
+};
+
+export default Header;
+
+const HeaderSection = styled.div``;
+
+const MainHeader = styled.div`
+  width: 80%;
+  margin: 16px auto;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HeaderImgs = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+const HeaderNavbarContainer = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+const HeaderNavbar = styled.ul`
+  display: flex;
+  gap: 25px;
+  align-items: center;
+  cursor: pointer;
+`;
+const ButtonContainer = styled.button`
+  background: #1a8917;
+  border-radius: 32px;
+  cursor: pointer;
+  border: none;
+  color: white;
+  width: 152px;
+  height: 40px;
+`;
