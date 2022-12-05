69ecd8dda819c1e9b717d8d2c46d097676481413
AniAvazneli <avazneli.ani@gmail.com>
2022-12-05 15:31:14 +0400

    feat: created Rating and PoliticalList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import styled from "styled-components";
 import Header from "./components/Header";
+import PoliticalList from "./components/political-list/PoliticalList";
+import "./font/TBCContractica-Bold.ttf"
 
 function App() {
   return (
-    <div>
+    <Main>
       <Header />
-    </div>
+      <PoliticalList/>
+    </Main>
   );
 }
 
 export default App;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`
